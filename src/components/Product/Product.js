df81135d8986e3df52f55999a00d8c3365f533c2
setykarimi <setykarimi80@gmail.com>
2022-08-04 19:19:49 +0430

    comment input

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -8,10 +8,10 @@ const Product = ({click,product,onDecrement,onIncrement,onChange,onDelete}) => {
   },[product.quantity] )  
     return (
         <div className={styles.product} onClick={click}>
-            <p>Product name: <b>{product.name}</b></p>
+            <p>Product name: <b>{product.title}</b></p>
             <p>Product price: <b>{product.price}</b></p>
             <span className={styles.quantity__value}>{product.quantity}</span>
-            <input className={styles.input} type="text" onChange={onChange} value={product.name }/>
+            {/* <input className={styles.input} type="text" onChange={onChange} value={product.name }/> */}
             <button className={`${styles.button} ${product.quantity == 1 && styles.remove}` } onClick={onDecrement}>
                 {product.quantity > 1 ? "-" : <BiTrashAlt/>}
             </button>
