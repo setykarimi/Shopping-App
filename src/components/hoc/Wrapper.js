c6a5de61a507a904bbd19b0d7cd18eb351c4105a
setykarimi <setykarimi80@gmail.com>
2022-08-04 18:30:08 +0430

    create new file for wrapper component

---
+++ src/components/hoc/Wrapper.js
@@ -0,0 +1,12 @@
+const Wrapper = (WrappedComponent , className) => {
+    return (props) => {
+        return (
+            <div className={className}>
+                <WrappedComponent {...props}/> 
+            </div>
+        )
+    }
+}
+ 
+ 
+export default Wrapper;
