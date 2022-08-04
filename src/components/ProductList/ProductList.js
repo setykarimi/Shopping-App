7e604fd29c9882c4ca9c1c594c72dec3959d6808
setykarimi <setykarimi80@gmail.com>
2022-08-04 18:28:37 +0430

    convert coponent to functional component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,36 +1,37 @@
 import Product from "../Product/Product";
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { useProducts, useProductsAction } from "../Providers/ProductsProvider";
 
-class ProductList extends React.Component {
-  renderProduct = () => {
-    const {onRemove , onIncrement , onChange , onDecrement , products} = this.props;
-    if(this.props.products.length === 0)
+
+const ProductList = (props) => {
+
+  const products = useProducts();
+  const {removeHandler , incrementhandler , decrementHandler , changeHandler} = useProductsAction();
+
+  const renderProduct = () => {
+    if (products.length === 0)
       return <div>there is no product in cart</div>
-    
-      return products.map((product, index) => {
-        return (
-          <Product
-            key={index}
-            product={product}
-            onDelete={() =>onRemove(product.id)}
-            onIncrement={() => onIncrement(product.id)}
-            onDecrement={() =>onDecrement(product.id)}
-            onChange={(e) => onChange(e, product.id)}
-          />
-        )
-      })
-  }
-  
 
-  render() {
-    const {products} = this.props;
-    return (
-      <>
-      {!products.length && <div>go to shopping</div>}
-        {this.renderProduct()}
-      </>
-    );
+    return products.map((product, index) => {
+      return (
+        <Product
+          key={index}
+          product={product}
+          onDelete={() => removeHandler(product.id)}
+          onIncrement={() => incrementhandler(product.id)}
+          onDecrement={() => decrementHandler(product.id)}
+          onChange={(e) => changeHandler(e, product.id)}
+        />
+      )
+    })
   }
+
+
+  return (<>
+    {!products.length && <div>go to shopping</div>}
+    {renderProduct()}
+  </>);
 }
+
 
 export default ProductList;
