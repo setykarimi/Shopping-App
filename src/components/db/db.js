6c610ab8cfdd28c043a8fbabf471db44b1134210
setykarimi <setykarimi80@gmail.com>
2022-08-04 19:15:56 +0430

    create new file for database data

---
+++ src/components/db/db.js
@@ -0,0 +1,88 @@
+export const productsData = [
+    {
+        "id": 1,
+        "title": "Fjallraven",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 2,
+        "title": "Mens Casual ",
+        "price": 22.3,
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket.",
+        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 3,
+        "title": "Mens Cotton Jacket",
+        "price": 55.99,
+        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions",
+        "quantity": 1
+    },
+    {
+        "id": 4,
+        "title": "Mens Casual Slim Fit",
+        "price": 15.99,
+        "description": "The color could be slightly different between on the screen and in practice.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 5,
+        "title": "John Hardy",
+        "price": 695,
+        "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl.",
+        "category": "jewelery",
+        "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 6,
+        "title": "Solid Gold",
+        "price": 168,
+        "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez",
+        "category": "jewelery",
+        "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 7,
+        "title": "White Gold",
+        "price": 9.99,
+        "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her",
+        "category": "jewelery",
+        "image": "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 8,
+        "title": "Pierced Owl Rose",
+        "price": 10.99,
+        "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+        "category": "jewelery",
+        "image": "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 9,
+        "title": "WD 2TB Elements",
+        "price": 64,
+        "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity",
+        "category": "electronics",
+        "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+        "quantity": 1
+    },
+    {
+        "id": 10,
+        "title": "SanDisk SSD PLUS",
+        "price": 109,
+        "description": "Easy upgrade for faster boot up, shutdown, application load and response",
+        "category": "electronics",
+        "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+        "quantity": 1
+    }]
